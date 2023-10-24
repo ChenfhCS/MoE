76b2a67403587e480c7ae8c55571b0e3006e3783
Fahao Chen <chenfh612@gmail.com>
2023-10-24 14:38:30 +0000

    update

--- my_examples/transformer-xl/train.py
+++ my_examples/transformer-xl/train.py
@@ -346,7 +346,7 @@ if args.fp16:
 #     model = model.to(device)
 if args.multi_gpu:
     if args.expert_parallel:
-        # model.cuda(local_rank)
+        model.cuda(local_rank)
         if local_rank == 0:
             print("Before initialize distributed group!")
         dist.init_process_group(backend='nccl',
@@ -357,7 +357,7 @@ if args.multi_gpu:
         if local_rank == 0:
             print("After initialize distributed group!")
         model = DDP(model, device_ids=[local_rank])
-        model.cuda(local_rank)
+        # model.cuda(local_rank)
     else:
         if args.gpu0_bsz >= 0:
             model = BalancedDataParallel(args.gpu0_bsz // args.batch_chunk,
