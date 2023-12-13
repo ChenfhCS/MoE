993c3fdc29120c7303c14946534397c33a1a2c37
Fahao Chen <chenfh612@gmail.com>
2023-12-13 06:07:32 +0000

    update

--- fmoe/layers.py
+++ fmoe/layers.py
@@ -281,7 +281,7 @@ class FMoE(nn.Module):
         time_costs = 0
         start_step =0
         num_experts = total_experts
-
+        
         # # ------------------------------------------------ save gate score ------------------------------------------------ # #
         save_gate_score = False
         if save_gate_score == True:
@@ -345,7 +345,7 @@ class FMoE(nn.Module):
                         workload_in_experts += num_tokens
                 self.workloads[i].append(workload_in_experts)
             if training_step == 200:
-                np.savez(f'./workloads/workloads_on_experts_xl/worker_expert{self.moe_rank}.npz', self.workloads)
+                np.savez(f'./workloads/workloads_on_experts_bert/worker_expert{self.moe_rank}.npz', self.workloads)
         # # ----------------------------------------------------------------------------------------------------------------- # #
 
 
@@ -360,7 +360,7 @@ class FMoE(nn.Module):
                         workload_in_experts += num_tokens
                 self.workloads_throttling[i].append(workload_in_experts)
             if training_step == 200:
-                np.savez(f'./workloads/workloads_on_experts_xl_throttling/worker_expert{self.moe_rank}.npz', self.workloads_throttling)
+                np.savez(f'./workloads/workloads_on_experts_bert_throttling/worker_expert{self.moe_rank}.npz', self.workloads_throttling)
         # # ----------------------------------------------------------------------------------------------------------------- # #
 
 
