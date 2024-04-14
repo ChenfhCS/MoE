648e0c8af66622dc4a21dd8dd7d005c34e4ba783
Fahao Chen <chenfh612@gmail.com>
2024-04-14 10:45:39 +0000

    fmoe update function

---
+++ examples/fmoe_update.sh
@@ -0,0 +1,3 @@
+cp /home/ubuntu/fastmoe/fmoe/transformer.py /opt/conda/envs/moe_test/lib/python3.9/site-packages/fastmoe-1.1.0-py3.9-linux-x86_64.egg/fmoe/
+cp /home/ubuntu/fastmoe/fmoe/layers.py /opt/conda/envs/moe_test/lib/python3.9/site-packages/fastmoe-1.1.0-py3.9-linux-x86_64.egg/fmoe/
+cp /home/ubuntu/fastmoe/fmoe/functions.py /opt/conda/envs/moe_test/lib/python3.9/site-packages/fastmoe-1.1.0-py3.9-linux-x86_64.egg/fmoe/
